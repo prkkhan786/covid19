9ee959d280b433b5c712333819f0c80e1af8d4c4
prkkhan786 <prkkhan1@gmail.com>
2020-05-29 20:16:58 +0530

    Updates

---
+++ precache-manifest.af90a72b6f2e5267e0473e51e0a2b194.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9acbf3333fc974ff4f70112b1777e66",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "fd6f0e54852eb5f992b8",
+    "url": "/covid19/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f7977c54f87029c99e51",
+    "url": "/covid19/static/css/main.8454fdc4.chunk.css"
+  },
+  {
+    "revision": "fd6f0e54852eb5f992b8",
+    "url": "/covid19/static/js/2.e9adb9d3.chunk.js"
+  },
+  {
+    "revision": "745374e0907cbc9ae847f64b8efd9f9d",
+    "url": "/covid19/static/js/2.e9adb9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7977c54f87029c99e51",
+    "url": "/covid19/static/js/main.652aef6c.chunk.js"
+  },
+  {
+    "revision": "303c368679e82efc2045",
+    "url": "/covid19/static/js/runtime-main.85f707d1.js"
+  }
+]);
